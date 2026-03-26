160e254e727949ff17a8faaa7656edadc8b26e21
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2026-03-26 20:37:56 +0700

    init: create spring boot backend GFI

---
+++ backend/src/test/java/com/gfi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
